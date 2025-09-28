343262768bd0dd2d7ba284cd84245f55b9073ba4
dev-medina29 <mmbedi@byupathway.edu>
2025-09-28 16:34:58 +0100

    save

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Fraction fract2 = new Fraction(5);
+        Console.WriteLine(fract2.GetFractionString());
+        Console.WriteLine(fract2.GetDecimalValue());
+
+        Fraction fract3 = new Fraction(1, 3);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        Fraction fract4 = new Fraction(1, 3);
+        Console.WriteLine(fract4.GetFractionString());
+        Console.WriteLine(fract4.GetDecimalValue());
     }
 }
